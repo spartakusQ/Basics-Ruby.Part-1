a68d40fbc4d005a73ff2a0c88292ad6f0f19b761
Unknown <spartakus1688@gmail.com>
2018-09-11 13:50:21 +0300

    Программа для определения свойст треугольника.

---
+++ right-triangle.rb
@@ -0,0 +1,21 @@
+puts 'Программа для определения свойст треугольника.'
+puts 'Введите сторону: a'
+  side_a = gets.chomp.to_i
+puts 'Введите сторону: b'
+  side_b = gets.chomp.to_i
+puts 'Введите сторону: c'
+  side_c = gets.chomp.to_i
+
+  catheter_a = side_a**2
+  catheter_b = side_b**2
+  hypotenuse = side_c**2
+  sum_catheter = catheter_a + catheter_b
+
+
+if sum_catheter == hypotenuse
+  puts  'Треугольник прямоугольный.'
+elsif catheter_a == catheter_b && catheter_b == hypotenuse && hypotenuse == catheter_a
+  puts 'Треугольник равнобедренный и равносторонний.'
+else catheter_a == catheter_b || catheter_b == hypotenuse
+  puts 'Треугольник равнобедренный.'
+end
