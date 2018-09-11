4f2ab0084c8b61e813645f641ce7e3741ce3eb04
Unknown <spartakus1688@gmail.com>
2018-09-11 13:02:32 +0300

    Программа которая вычисляет площадь треугольника.

---
+++ area-of-a-triangle.rb
@@ -0,0 +1,7 @@
+puts 'Введите значение основания треугольника:'
+  base = gets.chomp.to_f
+puts 'Введите значение высоты треугольника:'
+  height = gets.chomp.to_f
+  area_of_a_triangle = 0.5*base*height
+
+puts "Площадь треугольника равна: #{area_of_a_triangle}"
