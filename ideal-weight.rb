67c985fb7bd5132e9052fbb58cbe3fe6775efc60
Unknown <spartakus1688@gmail.com>
2018-09-12 16:48:22 +0300

    Поправил отступы внутри условий/блоков.

--- ideal-weight.rb
+++ ideal-weight.rb
@@ -4,7 +4,7 @@ puts 'Каков ваш рост?'
 height = gets.chomp.to_i
 weight = height - 110
 if weight <= 0
-puts 'Ваш вес уже оптимальный'
+  puts 'Ваш вес уже оптимальный'
 else
-puts "Уважаемый #{name} ваш вес равен #{weight}"
+  puts "Уважаемый #{name} ваш вес равен #{weight}"
 end
