c604ae1b2b400855927df041ca679b4238a29621
Unknown <spartakus1688@gmail.com>
2018-09-12 10:47:32 +0300

    Правки для программы вычисляющей площадь треугольника (убрал отступы начало строки, добавил пробелы вокруг оператора умножения)

--- area-of-a-triangle.rb
+++ area-of-a-triangle.rb
@@ -1,7 +1,7 @@
 puts 'Введите значение основания треугольника:'
-  base = gets.chomp.to_f
+base = gets.chomp.to_f
 puts 'Введите значение высоты треугольника:'
-  height = gets.chomp.to_f
-  area_of_a_triangle = 0.5*base*height
+height = gets.chomp.to_f
+area_of_a_triangle = 0.5 * base * height
 
 puts "Площадь треугольника равна: #{area_of_a_triangle}"
